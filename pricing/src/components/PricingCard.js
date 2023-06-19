7b9f9c464b42d26002dcf6963398ff43f24778c7
ChannappGadad <118685319+ChannappGadad@users.noreply.github.com>
2023-06-19 12:08:31 +0530

    Started with basics

---
+++ pricing/src/components/PricingCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PricingCard = () => {
+    return(
+        <div className="PricingApp">
+            <div className="app-container">
+                <h1>Pricing plans</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default PricingCard;
